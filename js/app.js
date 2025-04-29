9b8115e0c2480104a19b1d07d99bb6400b049536
Luna-bf <lburattifayolle@gmail.com>
2025-04-29 15:28:25 +0200

    Updated js file

--- js/app.js
+++ js/app.js
@@ -3,66 +3,7 @@ const elements = {
     darkMode: window.localStorage.getItem("dark-mode"),
     body: document.querySelector('body'),
     changeTheme: document.getElementById("change-theme"),
-    i: document.querySelector('#prompt-x'),
-    prompt: document.querySelector("#prompt"),
-    paragraph: document.querySelector("#prompt p"),
-    sidenav: document.getElementById("mySidenav"),
-    openBtn: document.getElementById("openBtn"),
-    closeBtn: document.getElementById("closeBtn"),
-    desktopNav: document.getElementById("space-between"),
-    mobileNav: document.getElementById("mySidenav"),
 }
-
-/*Dark mode (me laisse bloquée sur le mode sombre, trouver l'origine du problème)*/
-
-// function darkMode() {
-//   if (elements.body.classList.contains("")) {
-//     elements.body.classList.remove("dark-mode");
-//     localStorage.setItem("theme", "light");
-//   } else {
-//     elements.body.classList.add("dark-mode");
-//     localStorage.setItem("theme", "dark");
-//   }
-// }
-
-// if (localStorage.getItem("theme") === "dark") {
-//   elements.body.classList.add('dark-mode');
-
-//   elements.changeTheme.addEventListener('click', darkMode);
-// }
-
-// function darkMode() {
-//     let setTheme = document.body;
-//     setTheme.classList.toggle("dark-mode")
-//     let theme;
-
-//     if (setTheme.classList.contains("dark-mode")){
-//         console.log("Dark mode");
-//         theme = "Dark";
-    
-//     } else {
-//         console.log("Light mode");
-//         theme = "Light";
-//     }
-//     // save to localStorage
-//     localStorage.setItem("PageTheme", JSON.stringify(theme));
-//     // ensure you convert to JSON like i have done -----JSON.stringify(theme)
-// }
-
-// // Mauvais pratique, à changer
-// setInterval(() => {
-//     let getTheme = JSON.parse(localStorage.getItem("PageTheme"));
-    
-//     if(getTheme === "Dark"){
-//         document.body.classList = "dark-mode";
-//         clearInterval(getTheme);
-//     } else {
-//         document.body.classList = "";
-//         clearInterval(getTheme);
-//     }
-// }, 5);
-
-
 
 //Je déclare mes fonctions enableDarkMode et disableDarkMode
 const enableDarkMode = () => {
@@ -92,13 +33,3 @@ elements.changeTheme.addEventListener("click", () => {
 //     }
 //     */
 });
-
-//Apparition du prompt
-setTimeout(function() {
-     elements.prompt.classList.remove('hide-prompt');
- }, 8000);
- 
- //Disparition du prompt
-     elements.i.addEventListener('click', () => {
-     elements.prompt.classList.add('hide-prompt');
- });
