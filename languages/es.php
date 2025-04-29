8c9677a827418b0a6e397085796783fab87f14ce
Luna-bf <lburattifayolle@gmail.com>
2025-04-29 16:04:37 +0200

    Added languages

---
+++ languages/es.php
@@ -0,0 +1,14 @@
+<?php
+
+#A faire quand j'aurais fini les tâches plus importantes
+$lang = [
+    'Langue' => 'Idioma',
+    'Thème' => 'Tema',
+    'Cliquez pour changer de thème' => 'Haga clic para cambiar el tema',
+    'Projets' => 'Proyectos',
+    'Compétences' => 'Habilidades',
+    'Contact' => 'Contacto',
+    'Télécharger mon CV (FR)' => 'Subir mi CV (FR)',
+];
+
+?>
