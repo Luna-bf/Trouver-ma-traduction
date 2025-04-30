a9d80d918b9d1458054565839b36a20d0d49d3da
Luna-bf <lburattifayolle@gmail.com>
2025-04-30 13:15:52 +0200

    Deleted useless data

--- languages/es.php
+++ languages/es.php
@@ -2,13 +2,9 @@
 
 #A faire quand j'aurais fini les tâches plus importantes
 $lang = [
-    'Langue' => 'Idioma',
-    'Thème' => 'Tema',
-    'Cliquez pour changer de thème' => 'Haga clic para cambiar el tema',
     'Projets' => 'Proyectos',
     'Compétences' => 'Habilidades',
     'Contact' => 'Contacto',
-    'Télécharger mon CV (FR)' => 'Subir mi CV (FR)',
 ];
 
 ?>
