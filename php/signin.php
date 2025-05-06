daa3f8811de5c12dadd7f4d5a2673c6d39d4f431
Luna-bf <lburattifayolle@gmail.com>
2025-05-06 15:00:07 +0200

    Started working on the signin page

---
+++ php/signin.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Trouver ma traduction - Connexion</title>
+    <link rel="stylesheet" href="../css/form.css">
+</head>
+<body>
+    <form action="form.php">
+        <label for="Username-email">Nom d'utilisateur ou email</label>
+        <input type="text" class="form-input">
+    </form>
+</body>
+</html>
