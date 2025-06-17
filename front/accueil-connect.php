f6dc46c70040a5b9db8c7baa4a773484fa1e38fe
Luna-bf <lburattifayolle@gmail.com>
2025-06-17 10:23:15 +0200

    Connected landing page (unfinished)

---
+++ front/accueil-connect.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Accueil - Find My Translation</title>
+    <link rel="stylesheet" href="../css/style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css">
+</head>
+<body>
+    <header>
+        <nav>
+            <div id="space-between">
+                <div class="d-flex" id="search-logo">
+                    <a href="#" class="d-flex">
+                        <img src="../assets/imgs/logo.png" alt="website logo" id="website-logo-nav">
+                    </a>
+
+                    <input type="search" placeholder="Recherche...">
+                </div>
+
+                <ul class="d-flex" id="nav-ul">
+                    <li id="profile-picture"><img src="../assets/imgs/Greenpath-wallpaper.png" alt="Profile test"></li>
+                </ul>
+            </div>
+        </nav>
+    </header>
+</body>
+</html>
