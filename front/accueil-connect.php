004d1c8e1d7ecc226ab00ae724c51c925648bb2e
Luna-bf <lburattifayolle@gmail.com>
2025-07-03 16:36:42 +0200

    Updated accueil-connect

--- front/accueil-connect.php
+++ front/accueil-connect.php
@@ -9,21 +9,69 @@
 </head>
 <body>
     <header>
-        <nav>
-            <div id="space-between">
-                <div class="d-flex" id="search-logo">
-                    <a href="#" class="d-flex">
-                        <img src="../assets/imgs/logo.png" alt="website logo" id="website-logo-nav">
-                    </a>
+        <nav class="d-flex space-between">
+            <div class="d-flex logo-input">
+                <!-- <figure>
+                    <img src="../assets/imgs/logo.png" alt="website logo" id="website-logo-nav">
+                </figure> -->
+                <a href="#">Logo</a>
 
+                <div class="input-wrapper">
                     <input type="search" placeholder="Recherche...">
                 </div>
-
-                <ul class="d-flex" id="nav-ul">
-                    <li id="profile-picture"><img src="../assets/imgs/Greenpath-wallpaper.png" alt="Profile test"></li>
-                </ul>
             </div>
+
+            <ul class="d-flex" id="nav-ul">
+                <li id="profile-picture"><img src="../assets/imgs/profile-picture-example.png" alt="Profile test"></li>
+            </ul>
         </nav>
     </header>
+
+    <main>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas, tempore sapiente?</p>
+    </main>
+
+    <footer class="d-flex">
+        <div class="d-flex logo-text margin-foot">
+            <div class="parent-logo">
+                <a href="#">
+                    <img src="../assets/imgs/logo.png" alt="website logo" id="website-logo-footer">
+                </a>
+            </div>
+
+            <div class="d-flex parent-text">
+                <div class="text">
+                    <p>Ce site a été réalisé dans le cadre de la formation "TP - Développeur Web" de l'AFEC.</p>
+                </div>
+            </div>
+        </div>
+
+        <hr>
+
+        <div class="d-flex socials-pages margin-foot">
+            <div class="d-flex parent-pages">
+                <h3 id="pages-title">Toutes les pages</h3>
+                <ul class="pages">
+                    <li><a href="accueil.php">Accueil du site</a></li>
+                    <li><a href="#">Accueil (traductions)</a></li>
+                    <li><a href="#">Profil</a></li>
+                </ul>
+            </div>
+                    
+            <div class="d-flex parent-socials margin-foot">
+                <h3 id="socials-title">Nos réseaux sociaux</h3>
+                
+                <ul class="socials">
+                    <li><a href="https://www.facebook.com/?locale=fr_FR" target="_blank"><img src="../assets/imgs/icons/facebook-logo.png" alt="Facebook logo" class="facebook"></a></li>
+                    <li><a href="https://www.instagram.com/" target="_blank"><img src="../assets/imgs/icons/instagram-logo.png" alt="Instagram logo" class="instagram"></a></li>
+                    <li><a href="https://www.youtube.com/" target="_blank"><img src="../assets/imgs/icons/youtube-logo.png" alt="Youtube logo" class="youtube"></a></li>
+                </ul>
+            </div>
+        </div>
+
+        <hr>
+
+        <p class="freepik">Toutes les images viennent de <a href="https://www.freepik.com/" target="_blank">Freepik</a></p>
+    </footer>
 </body>
 </html>
