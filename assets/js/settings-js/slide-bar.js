458fa9dc96bc27b9e5c42a69f9a652f92f71e13c
Luna-bf <lburattifayolle@gmail.com>
2025-08-10 17:57:48 +0200

    Updated code

--- assets/js/settings-js/slide-bar.js
+++ assets/js/settings-js/slide-bar.js
@@ -1,7 +1,8 @@
 let slider = document.getElementById("myRange");
 let output = document.getElementById("number-input");
-output.textContent = slider.value;
 
+// Je voudrais lier la valeur de l'input number et celle de range pour que si je change l'une des deux, l'autre change aussi
+// Lien utile ? : https://stackoverflow.com/questions/25825843/restrict-user-to-put-value-in-range-in-html-input-type-number
 slider.oninput = function () {
-    output.textContent = `Taille actuelle: ${this.value}px`;
+    output.value = this.value;
 }
