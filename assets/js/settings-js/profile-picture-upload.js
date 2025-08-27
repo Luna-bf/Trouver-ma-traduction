f666b3d16bb84c04e7f3106ced2bd9f00195eac0
Luna-bf <lburattifayolle@gmail.com>
2025-08-27 13:52:25 +0200

    Added a file preview and restrictions

--- assets/js/settings-js/profile-picture-upload.js
+++ assets/js/settings-js/profile-picture-upload.js
@@ -1,15 +1,40 @@
-const uploadFileElements = {
-  inputFile: document.querySelector('.buttons #choose-file-button #choose'),
-  filePreview: document.querySelector('.preview figure img'),
-  previewName: document.querySelector('p .preview-name'),
-  previewFormat: document.querySelector('p .preview-format'),
-}
+// DOM ELEMENTS
+const inputFile = document.querySelector('.choose-container>#choose-image');
+const filePreview = document.querySelector('.preview figure>img');
+const fileSizeMessage = document.querySelector('.choose-container>.img-restrictions');
+// const filePreview = document.querySelector('.preview>#image');
+
+// CONSTS/VARS
+const maxSize = 10 * 1024;
+
+// ARROW FUNCTIONS
+const changePreviewOnUpload = (fileSize, fileType, previewURL) => { // Je passe les clés de l'objet fileObjet en tant que param
+
+    if (fileType.includes("image") && fileSize > maxSize) {
+      fileSizeMessage.textContent = `Le poids du fichier ne dois pas dépasser ${maxSize}Kb (1280 octets).`;
+      fileSizeMessage.style.display = 'block';
+
+      // filePreview.style.backgroundImage = previewURL;
+
+    } else if(!fileType.includes("image")) {
+      fileSizeMessage.textContent = 'Le format du fichier doit être de type "image".';
+      fileSizeMessage.style.display = 'block';
+
+    } else {
+      filePreview.setAttribute("src", previewURL);
+      fileSizeMessage.style.display = 'none';
+    }
+  }
 
 
-uploadFileElements.inputFile.addEventListener('change', function() {
+// EVENT LISTENER
+inputFile.addEventListener('change', () => {
 
-  // .files sert à indiquer que l'élément inputFile renverra un fichier
-  const fileObject = uploadFileElements.inputFile.files[0];
+  const fileObject = inputFile.files[0]; // .files sert à indiquer que l'élément inputFile renverra un fichier
 
-  console.log(fileObject); // Renvoie un objet FileList contenant toutes les infos du fichier : nom, date de modification, sa taille et son format
+  const objectURL = URL.createObjectURL(fileObject);
+  console.log(objectURL); // Renvoie un lien "blob"
+  // console.log(fileObject); // Renvoie un objet FileList contenant toutes les infos du fichier : nom, date de modification, sa taille et son format
+
+  changePreviewOnUpload(fileObject.size, fileObject.type, objectURL);
 });
