73c4f2d026469920bccc522144d5eea9d1fab5f9
Luna-bf <lburattifayolle@gmail.com>
2025-12-15 09:49:37 +0100

    Création de fichiers contenant les éléments qui se répètent dans sur une ou plusieurs pages : les slides du carousel, le footer et le header, ainsi que les cartes simples présentent dans les pages search-result et profil

---
+++ views/components/_swiper-slide.php
@@ -0,0 +1,156 @@
+<div class="swiper-slide"> <!-- Une seule carte, cette classe permet à Swiper.js de faire défiler les cartes -->
+    <div class="card-content"> <!-- Le contenu de la carte -->
+        <figure><img src="/assets/imgs/general/slider/pdf-file-extension.png"
+                alt="PDF file extension" />
+        </figure>
+
+        <div class="title">
+            <h3>Nom de la traduction</h3>
+            <h4 class="see-translation-link"><a href="file-viewer.php">Voir la traduction</a>
+            </h4>
+        </div>
+
+        <ul class="text">
+            <li>Type : (Type de contenu)</li>
+            <li>Style : (Style)</li>
+            <li>Auteur : (Nom de l'auteur)</li>
+            <li>Langue : (Langue)</li>
+        </ul>
+
+        <ul class="other-infos">
+            <li>Par : Nom de l'utilisateur</li>
+            <li>Postée le : JJ/MM/AAAA</li>
+        </ul>
+    </div>
+</div>
+
+<div class="swiper-slide">
+    <div class="card-content">
+        <figure><img src="/assets/imgs/general/slider/docx-file-extension.png"
+                alt="PDF file extension" />
+        </figure>
+
+        <div class="title">
+            <h3>Nom de la traduction</h3>
+            <h4 class="see-translation-link"><a href="file-viewer.php">Voir la traduction</a>
+            </h4>
+        </div>
+
+        <ul class="text">
+            <li>Type : (Type de contenu)</li>
+            <li>Style : (Style)</li>
+            <li>Auteur : (Nom de l'auteur)</li>
+            <li>Langue : (Langue)</li>
+        </ul>
+
+        <ul class="other-infos">
+            <li>Par : Nom de l'utilisateur</li>
+            <li>Postée le : JJ/MM/AAAA</li>
+        </ul>
+    </div>
+</div>
+
+<div class="swiper-slide">
+    <div class="card-content">
+        <figure><img src="/assets/imgs/general/slider/pdf-file-extension.png"
+                alt="PDF file extension" />
+        </figure>
+
+        <div class="title">
+            <h3>Nom de la traduction</h3>
+            <h4 class="see-translation-link"><a href="file-viewer.php">Voir la traduction</a>
+            </h4>
+        </div>
+
+        <ul class="text">
+            <li>Type : (Type de contenu)</li>
+            <li>Style : (Style)</li>
+            <li>Auteur : (Nom de l'auteur)</li>
+            <li>Langue : (Langue)</li>
+        </ul>
+
+        <ul class="other-infos">
+            <li>Par : Nom de l'utilisateur</li>
+            <li>Postée le : JJ/MM/AAAA</li>
+        </ul>
+    </div>
+</div>
+
+<div class="swiper-slide">
+    <div class="card-content">
+        <figure><img src="/assets/imgs/general/slider/docx-file-extension.png"
+                alt="PDF file extension" />
+        </figure>
+
+        <div class="title">
+            <h3>Nom de la traduction</h3>
+            <h4 class="see-translation-link"><a href="file-viewer.php">Voir la traduction</a>
+            </h4>
+        </div>
+
+        <ul class="text">
+            <li>Type : (Type de contenu)</li>
+            <li>Style : (Style)</li>
+            <li>Auteur : (Nom de l'auteur)</li>
+            <li>Langue : (Langue)</li>
+        </ul>
+
+        <ul class="other-infos">
+            <li>Par : Nom de l'utilisateur</li>
+            <li>Postée le : JJ/MM/AAAA</li>
+        </ul>
+    </div>
+</div>
+
+<div class="swiper-slide">
+    <div class="card-content">
+        <figure><img src="/assets/imgs/general/slider/pdf-file-extension.png"
+                alt="PDF file extension" />
+        </figure>
+
+        <div class="title">
+            <h3>Nom de la traduction</h3>
+            <h4 class="see-translation-link"><a href="file-viewer.php">Voir la traduction</a>
+            </h4>
+        </div>
+
+        <ul class="text">
+            <li>Type : (Type de contenu)</li>
+            <li>Style : (Style)</li>
+            <li>Auteur : (Nom de l'auteur)</li>
+            <li>Langue : (Langue)</li>
+        </ul>
+
+        <ul class="other-infos">
+            <li>Par : Nom de l'utilisateur</li>
+            <li>Postée le : JJ/MM/AAAA</li>
+        </ul>
+    </div>
+</div>
+
+<div class="swiper-slide">
+    <div class="card-content">
+        <figure><img src="/assets/imgs/general/slider/docx-file-extension.png"
+                alt="PDF file extension" />
+        </figure>
+
+        <div class="title">
+            <h3>Nom de la traduction</h3>
+            <h4 class="see-translation-link"><a href="file-viewer.php">Voir la traduction</a>
+            </h4>
+        </div>
+
+        <ul class="text">
+            <li>Type : (Type de contenu)</li>
+            <li>Style : (Style)</li>
+            <li>Auteur : (Nom de l'auteur)</li>
+            <li>Langue : (Langue)</li>
+        </ul>
+
+        <ul class="other-infos">
+            <li>Par : Nom de l'utilisateur</li>
+            <li>Postée le : JJ/MM/AAAA</li>
+        </ul>
+    </div>
+</div>
+</div>
