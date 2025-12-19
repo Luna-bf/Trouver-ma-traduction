d798a53f09fa0da83182139ca4fd891f0772e56f
Luna-bf <lburattifayolle@gmail.com>
2025-12-19 12:50:02 +0100

    Mise à jour du nom du dossier

---
+++ views/inc/_footer.php
@@ -0,0 +1,17 @@
+<footer class="d-flex">
+    <div class="d-flex logo-text margin-foot">
+        <figure class="parent-logo">
+            <a href="#"><img src="/assets/imgs/logo-160x160.png" alt="website logo"></a>
+        </figure>
+
+        <div class="d-flex parent-text">
+            <div class="text">
+                <p>Ce site a été réalisé dans le cadre de la formation "TP - Développeur Web" de l'AFEC.</p>
+            </div>
+        </div>
+    </div>
+
+    <hr>
+
+    <p class="freepik">Toutes les images viennent de <a href="https://www.freepik.com/" target="_blank">Freepik</a>
+</footer>
---
+++ views/inc/_header.php
@@ -0,0 +1,29 @@
+<body>
+    <div id="cover-background"></div>
+
+    <header>
+        <nav class="d-flex">
+            <figure class="nav-logo-figure">
+                <a href="#"><img src="/assets/imgs/logo-160x160.png" alt="Website logo" class="nav-logo-img"></a>
+            </figure>
+
+            <input type="checkbox" id="check">
+
+            <div class="d-flex input-and-profile">
+                <label for="check" class="close-menu"><i class="fa-solid fa-xmark"></i></label>
+
+                <form action="" class="search-form">
+                    <div class="d-flex input-wrapper">
+                        <input type="search" class="input-icon" placeholder="&#xf002;&ensp;&ensp;Recherche...">
+                    </div>
+                </form>
+
+                <figure class="d-flex profile-picture-figure">
+                    <a href="/views/profile.php"><img src="/assets/imgs/placeholders/default-pfp.png"
+                            alt="Profile picture"></a>
+                </figure>
+            </div>
+
+            <label for="check" class="open-menu"><i class="fa-solid fa-bars"></i></label>
+        </nav>
+    </header>
