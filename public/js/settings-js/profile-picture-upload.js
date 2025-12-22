eeeaeaf657bc48c075b3456b38f0aed0576f9b1f
Luna-bf <lburattifayolle@gmail.com>
2025-12-22 15:56:31 +0100

    Déplacement du dossier 'js' dans le dossier 'public' (pour l'architecture MVC)

---
+++ public/js/settings-js/profile-picture-upload.js
@@ -0,0 +1,42 @@
+// DOM ELEMENTS
+const inputFile = document.querySelector('.choose-container>#choose-image');
+const filePreview = document.querySelector('.preview figure>img');
+const fileSizeMessage = document.querySelector('.choose-container>#img-restrictions');
+// const filePreview = document.querySelector('.preview>#image');
+
+// CONSTS/VARS
+const maxSize = 10 * 1024;
+
+// ARROW FUNCTIONS
+const changePreviewOnUpload = (fileSize, fileType, previewURL) => { // Je passe les clés de l'objet fileObjet en tant que param
+
+    if (fileType.includes("image") && fileSize > maxSize) {
+      filePreview.setAttribute("src", "../../../assets/imgs/placeholders/placeholder-picture.png");
+      fileSizeMessage.textContent = `Le poids du fichier ne dois pas dépasser ${maxSize}Kb (1280 octets).`;
+      fileSizeMessage.style.display = 'block';
+
+      // filePreview.style.backgroundImage = previewURL;
+
+    } else if(!fileType.includes("image")) {
+      filePreview.setAttribute("src", "../../../assets/imgs/placeholders/placeholder-picture.png");
+      fileSizeMessage.textContent = 'Le format du fichier doit être de type "image".';
+      fileSizeMessage.style.display = 'block';
+
+    } else {
+      filePreview.setAttribute("src", previewURL);
+      fileSizeMessage.style.display = 'none';
+    }
+  }
+
+
+// EVENT LISTENER
+inputFile.addEventListener('change', () => {
+
+  const fileObject = inputFile.files[0]; // .files sert à indiquer que l'élément inputFile renverra un fichier
+
+  const objectURL = URL.createObjectURL(fileObject);
+  console.log(objectURL); // Renvoie un lien "blob"
+  // console.log(fileObject); // Renvoie un objet FileList contenant toutes les infos du fichier : nom, date de modification, sa taille et son format
+
+  changePreviewOnUpload(fileObject.size, fileObject.type, objectURL);
+});
---
+++ public/js/slider.js
@@ -0,0 +1,27 @@
+new Swiper('.cards-wrapper', {
+    loop: true,
+
+    // Flèches de navigation
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+    // Responsive (min-width)
+    breakpoints: {
+        
+        0: { // Ses paramètres s'appliquent lorsque la largeur de l'écran est supérieure ou égale à 0px (soit de 0px à 575px)
+            slidesPerView: 1,
+        },
+
+        576: { // Ses paramètres s'appliquent lorsque la largeur de l'écran est supérieure ou égale à 576px (soit de 576px à 1023px)
+            slidesPerView: 2,
+            spaceBetween: 25,
+        },
+
+        1024: { // Ses paramètres s'appliquent lorsque la largeur de l'écran est supérieure ou égale à 1024px
+            slidesPerView: 4,
+            spaceBetween: 50,
+        },
+    }
+});
